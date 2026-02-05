95d9925b249ac68eb59b806371a408bdf47be0d0
Miguel Ángel de la Cruz Lázaro <256243310+Miguel-Dark@users.noreply.github.com>
2026-02-05 17:12:29 -0600

    Estructura organizada con carpeta src y primer ejercicio listo

--- AgregandoElementos.java
+++
@@ -1,5 +0,0 @@
-public class AgregandoElementos {
-    public static void main(String[] args) {
-        System.out.println("¡A la porra IntelliJ, yo mando aquí!");
-    }
-}
---
+++ src/AgregandoElementos.java
@@ -0,0 +1,2 @@
+public class AgregandoElementos {
+}
