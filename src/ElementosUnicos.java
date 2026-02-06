80cfda546b30755f9687d78b47c1449b36da2a4b
Miguel Ángel de la Cruz Lázaro <256243310+Miguel-Dark@users.noreply.github.com>
2026-02-05 20:39:59 -0600

    Ejercicio 4: Uso de HashSet para eliminar eventos duplicados de forma automática

---
+++ src/ElementosUnicos.java
@@ -0,0 +1,16 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class ElementosUnicos {
+    public static void main(String[] args) {
+        Set<String> eventos = new HashSet<>();
+        eventos.add("IA Conference");
+        eventos.add("AI Summit");
+        eventos.add("DevFest");
+        eventos.add("Cloud Expo");
+        eventos.add("IA Conference");
+        eventos.add("DevFest");
+
+        System.out.println("Lista de eventos: "+eventos);
+    }
+}
