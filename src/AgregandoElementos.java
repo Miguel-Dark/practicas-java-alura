3a0aea1b0a4c61e09bf4124343bf3e65ffcf81ee
Miguel Ángel de la Cruz Lázaro <256243310+Miguel-Dark@users.noreply.github.com>
2026-02-05 19:06:54 -0600

    ¡Victoria! Estructura src configurada y primer ejercicio funcionando

--- src/AgregandoElementos.java
+++ src/AgregandoElementos.java
@@ -1,2 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class AgregandoElementos {
+    public static void main(String[] args) {
+        List<String> empleados = new ArrayList<>();
+        empleados.add("Juan");
+        empleados.add("María");
+        empleados.add("Carlos");
+        empleados.add("Ana");
+        System.out.println("Lista de emnpleados: "+empleados);
+    }
 }
